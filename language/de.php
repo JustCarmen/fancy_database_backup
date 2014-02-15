1040cbe9b197dee2c89ada995b87c04f4d2351f6
JustCarmen <carmen@justcarmen.nl>
2014-02-15 22:20:32 +0100

    Added German language by Hermann.

---
+++ language/de.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('WT_WEBTREES')) {
+header('HTTP/1.0 403 Forbidden');
+exit;
+}
+
+return array(
+	'Provides access to MySQLDumper. A database backup tool.' => 'Ermöglicht die integrierte Nutzung des Datenbank-BackUp-Programms MySQLDumper.',
+	'Sorry, your browser does not support iframes.' => 'Browser unterstützt leider keine iframes.'
+);
