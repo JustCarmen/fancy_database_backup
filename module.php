daf89dce6da7daed1c1c792ef33473538aaef313
JustCarmen <carmen@justcarmen.nl>
2015-01-23 08:12:05 +0100

    New admin theme

--- module.php
+++ module.php
@@ -28,20 +28,20 @@ class fancy_database_backup_WT_Module extends WT_Module implements WT_Module_Con
 	public function __construct() {
 		parent::__construct();
 		// Load any local user translations
-		if (is_dir(WT_MODULES_DIR.$this->getName().'/language')) {
-			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.mo')) {
+		if (is_dir(WT_MODULES_DIR . $this->getName() . '/language')) {
+			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.mo')) {
 				WT_I18N::addTranslation(
-					new Zend_Translate('gettext', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.mo', WT_LOCALE)
+					new Zend_Translate('gettext', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.mo', WT_LOCALE)
 				);
 			}
-			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.php')) {
+			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.php')) {
 				WT_I18N::addTranslation(
-					new Zend_Translate('array', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.php', WT_LOCALE)
+					new Zend_Translate('array', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.php', WT_LOCALE)
 				);
 			}
-			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.csv')) {
+			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.csv')) {
 				WT_I18N::addTranslation(
-					new Zend_Translate('csv', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.csv', WT_LOCALE)
+					new Zend_Translate('csv', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.csv', WT_LOCALE)
 				);
 			}
 		}
@@ -59,26 +59,32 @@ class fancy_database_backup_WT_Module extends WT_Module implements WT_Module_Con
 
 	// Extend WT_Module
 	public function modAction($mod_action) {
-		switch($mod_action) {
-		case 'admin':
-			$controller=new WT_Controller_Page();
-			$controller
-				->restrictAccess(Auth::isAdmin())
-				->pageHeader();
-
-			echo '<div id="fancy_db">';
-			echo '<iframe src="mysqldumper/index.php" width="100%" height="580">'; // Change this src link if you have not installed MySQLDumper in the webtrees root.
-			echo '<p>'.WT_I18N::translate('Sorry, your browser does not support iframes.').'</p>';
-			echo '</iframe>';
-			echo '</div>';
-			break;
-		default:
-			header('HTTP/1.0 404 Not Found');
+		switch ($mod_action) {
+			case 'admin':
+				$controller = new WT_Controller_Page();
+				$controller
+					->restrictAccess(Auth::isAdmin())
+					->pageHeader();
+				?>
+				<ol class="breadcrumb small">
+					<li><a href="admin.php"><?php echo WT_I18N::translate('Administration'); ?></a></li>
+					<li><a href="admin_modules.php"><?php echo WT_I18N::translate('Module administration'); ?></a></li>
+					<li class="active"><?php echo $this->getTitle(); ?></li>
+				</ol>
+				<h2><?php echo $this->getTitle(); ?></h2>
+				<iframe src="mysqldumper/index.php" width="100%" height="580" style="border: 1px solid #ddd">
+				<p class="alert alert-danger"><?php echo WT_I18N::translate('Sorry, your browser does not support iframes.') ?></p>
+				</iframe>
+				<?php
+				break;
+			default:
+				header('HTTP/1.0 404 Not Found');
 		}
 	}
 
 	// Implement WT_Module_Config
 	public function getConfigLink() {
-		return 'module.php?mod='.$this->getName().'&amp;mod_action=admin';
+		return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin';
 	}
+
 }
