d0504a60f47ab5514756f074af7c77c7644f1587
JustCarmen <carmen@justcarmen.nl>
2015-03-30 06:17:50 +0200

    Module API changed.

--- module.php
+++ module.php
@@ -19,7 +19,7 @@ namespace Fisharebest\Webtrees;
 
 use Zend_Translate;
 
-class FancyDatabaseBackupModule extends Module implements ModuleConfigInterface {
+class FancyDatabaseBackupModule extends AbstractModule implements ModuleConfigInterface {
 
 	public function __construct() {
 		parent::__construct('fancy_database_backup');
