e7da8b97076b34dd9349431f8706541c14263dce
JustCarmen <carmen@justcarmen.nl>
2015-07-04 20:44:46 +0200

    Refactor and update language files

--- language/de.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-return array(
-	'Provides access to MySQLDumper. A database backup tool.' => 'Ermöglicht die integrierte Nutzung des Datenbank-BackUp-Programms MySQLDumper.',
-	'Sorry, your browser does not support iframes.' => 'Browser unterstützt leider keine iframes.'
-);
--- language/en-US.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'Provides access to MySQLDumper. A database backup tool.' => 'Provides access to MySQLDumper. A database backup tool.',
-	'Sorry, your browser does not support iframes.' => 'Sorry, your browser does not support iframes.'
-);
