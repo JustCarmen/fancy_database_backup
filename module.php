2625a2e4d568d693115f63c25ce5967765739f03
JustCarmen <carmen@justcarmen.nl>
2017-03-18 22:18:38 +0100

    Update version number

--- module.php
+++ module.php
@@ -24,7 +24,7 @@ use JustCarmen\WebtreesAddOns\FancyDatabaseBackup\Template\AdminTemplate;
 
 class FancyDatabaseBackupModule extends AbstractModule implements ModuleConfigInterface {
 
-	const CUSTOM_VERSION	 = '1.7.9';
+	const CUSTOM_VERSION	 = '1.8.0-dev';
 	const CUSTOM_WEBSITE	 = 'http://www.justcarmen.nl/fancy-modules/fancy-database-backup/';
 
 	public function __construct() {
